d974eea1fd1ccfd26f6b3a213c95cde5067c1a70
Gastón Valentini <gastonvalentiniruiz@gmail.com>
2023-10-04 18:14:22 +0200

    Added dynamic rendering of the color selector in play.html.

--- viewes/play/play.js
+++ viewes/play/play.js
@@ -1,2 +1,10 @@
-let info = localStorage.getItem("info")
-console.log(info)
+let info = JSON.parse(localStorage.getItem("info"))
+
+let colorPicker = document.querySelector(".colorPicker")
+
+info.colors.forEach(e => {
+    let colorSelector = document.createElement("div")
+    colorSelector.style.backgroundColor = e
+    colorSelector.classList.add("colorPickerButton")
+    colorPicker.appendChild(colorSelector)
+});
