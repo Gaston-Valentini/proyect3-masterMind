43503f1fbdffeae82baefaed5180be2ddd53d51f
Gastón Valentini <gastonvalentiniruiz@gmail.com>
2023-10-06 19:15:57 +0200

    Fixed a bug in the automatic rendering of the board based on the difficulty in play.html.

--- viewes/play/play.js
+++ viewes/play/play.js
@@ -1,33 +1,41 @@
 let colors = JSON.parse(localStorage.getItem("info")).colors
 let difficulty = JSON.parse(localStorage.getItem("info")).difficulty
 let difficultyNum = JSON.parse(localStorage.getItem("info")).difficultyNum
+let quantity = JSON.parse(localStorage.getItem("info")).quantity
 
 let colorPicker = document.querySelector(".colorPicker")
 let board = document.querySelector(".board")
 
 colors.forEach(e => {
-    let colorSelector = document.createElement("div")
-    colorSelector.style.backgroundColor = e
-    colorSelector.classList.add("colorPickerButton")
-    colorPicker.appendChild(colorSelector)
+    let colorPickerButton = document.createElement("div")
+    colorPickerButton.style.backgroundColor = e
+    colorPickerButton.classList.add("colorPickerButton")
+    colorPicker.appendChild(colorPickerButton)
 });
 
+const createRow = () => {
+    let boardRow = document.createElement("div")
+    boardRow.classList.add("boardRow")
+    let boardRowSelection = document.createElement("div")
+    boardRowSelection.classList.add("boardRowSelection")
+    let boardRowComprobation = document.createElement("boardRowComprobation")
+    boardRowComprobation.classList.add("boardRowComprobation")
+
+    boardRow.appendChild(boardRowSelection)
+    boardRow.appendChild(boardRowComprobation)
+
+    board.appendChild(boardRow)
+
+    for (let i = 0; i < quantity; i++) {
+        let boardRowSelectionColor = document.createElement("div")
+        boardRowSelectionColor.classList.add("boardRowSelectionColor")
+        let boardRowComprobationColor = document.createElement("div")
+        boardRowComprobationColor.classList.add("boardRowComprobationColor")
+        boardRowSelection.appendChild(boardRowSelectionColor)
+        boardRowComprobation.appendChild(boardRowComprobationColor)
+    }
+}
+
 for (let i = 0; i < difficultyNum; i++) {
-    let row = document.createElement("div")
-    row.classList.add("boardRow")
-    row.innerHTML = `
-        <div class="boardRowSelection">
-            <div class="boardRowSelectionColor"></div>
-            <div class="boardRowSelectionColor"></div>
-            <div class="boardRowSelectionColor"></div>
-            <div class="boardRowSelectionColor"></div>
-        </div>
-        <div class="boardRowComprobation">
-            <div class="boardRowComprobationColor"></div>
-            <div class="boardRowComprobationColor"></div>
-            <div class="boardRowComprobationColor"></div>
-            <div class="boardRowComprobationColor"></div>
-        </div>
-    `
-    board.appendChild(row)
+    createRow()
 }
